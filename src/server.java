26a257a024bc9cc65c08411962cb6394f16c816e
palaksharma1011 <palak16306@gmail.com>
2026-03-24 01:30:18 +0530

    fake key generating successfully

--- src/server.java
+++ src/server.java
@@ -1,6 +1,16 @@
 import java.net.*;
+import java.security.KeyFactory;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.MessageDigest;
+import java.security.PublicKey;
+import java.security.spec.X509EncodedKeySpec;
 import java.io.*;
 import java.util.*;
+
+import javax.crypto.KeyAgreement;
+import javax.crypto.SecretKey;
+import javax.crypto.spec.SecretKeySpec;
 
 public class server{
 
@@ -48,7 +58,20 @@ public class server{
                         // step 6 : broadcast the message to every client 
                 while((message = in.readLine()) != null){
                     System.out.println("[" + username + "] → " + message);
+
+                    
+                        // generating fake key
+                        KeyPair kp=genFakeKeyPair();
+                        SecretKey fakeKey=genFakePrivateKey(kp,genFakePublicKey(kp));
+
+                        byte[] keyBytes = fakeKey.getEncoded();
+                        String base64FakeKey = Base64.getEncoder().encodeToString(keyBytes);
+
+                        System.out.println("AES Key (Base64): " + base64FakeKey);
+
                     if(message.startsWith("KEY:")){
+
+
                         this.publicKey = message;
                         synchronized(clients){
                             for(ClientHandler client : clients){
@@ -71,6 +94,8 @@ public class server{
                             for(ClientHandler client : clients){
                                 if(client.username.equals(receiver)){
                                     client.sendMessage("MSG:" + username + ":" + parts[2]);
+                                    client.sendMessage(message);
+
                                     break;
                                 }
                             }
@@ -103,6 +128,7 @@ public class server{
                     // normal messages
                     // for everyone
                     String fullMessage = "[" + username + "]: " + message;
+
                     server.broadcast(fullMessage, this);
 
                 }
@@ -167,6 +193,64 @@ public class server{
             e.printStackTrace();
         }
 
+    }
+    public static String genFakePublicKey(KeyPair keyPair){
+
+        try{
+        KeyAgreement keyAgree = KeyAgreement.getInstance("DH");
+        keyAgree.init(keyPair.getPrivate());
+
+        // step 3 : Send public key
+        byte[] publicKeyBytes = keyPair.getPublic().getEncoded();
+        String publicKeyBase64=Base64.getEncoder().encodeToString(publicKeyBytes);
+        return publicKeyBase64;
+        }catch(Exception e){
+            throw new RuntimeException("Failed to generate public key", e);
+        }
+    }
+    
+    public static KeyPair genFakeKeyPair(){
+
+        try{
+        KeyPairGenerator keyGen= KeyPairGenerator.getInstance("DH");
+        keyGen.initialize(2048);
+
+        KeyPair keyPair = keyGen.generateKeyPair();
+
+        return keyPair;
+        }catch(Exception e){
+            throw new RuntimeException("Failed to generate key Pair", e);
+        }
+
+
+    }   
+    public static SecretKey genFakePrivateKey(KeyPair keyPair,String keyBase64){
+                            try{
+                                byte[] keyBytes = Base64.getDecoder().decode(keyBase64);
+
+                                KeyFactory keyFactory = KeyFactory.getInstance("DH");
+                                X509EncodedKeySpec keySpec = new X509EncodedKeySpec(keyBytes);
+                                PublicKey otherPublicKey = keyFactory.generatePublic(keySpec);
+
+                                
+                                // NEW CHANGE: creating new keyAgreement (fresh state)
+                                KeyAgreement ka=KeyAgreement.getInstance("DH");
+                                ka.init(keyPair.getPrivate());
+
+                                ka.doPhase(otherPublicKey,true);
+                                byte[] sharedSecret=ka.generateSecret();
+
+                                 MessageDigest sha = MessageDigest.getInstance("SHA-256");
+                            byte[] hash = sha.digest(sharedSecret);
+
+                            // Use first 16 bytes for AES-128
+                            SecretKey aesKey = new SecretKeySpec(hash, 0, 16, "AES");
+                            return aesKey;
+
+                            }
+                            catch(Exception e){
+                                 throw new RuntimeException("Failed to generate shared secret key", e);
+                            }
     }
 }
         // step 1 : start the server 
